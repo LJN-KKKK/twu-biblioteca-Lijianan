1365681dbefc09a7cecfb4f022deefa603ddd8f6
Jianan.Li <514068373@qq.com>
2020-08-05 15:24:30 +0800

    1.1 Add welcome message

---
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void showWelcomeMessageTest() {
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", BibliotecaApp.showWelcomeMessage());
+    }
+}
